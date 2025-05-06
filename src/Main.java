f55f05522f556cf31aada76a65d8c214a4065fca
EddieNine <believefixed@hotmail.com>
2025-05-06 19:51:38 -0300

    Primeiro programa em java.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Estou aprendendo Java!");
+        System.out.print("\nIsso é muito legal");
+
+        // Este é meu primeiro programa em java
+
+        /*
+        Isso é um comentario
+        de varias linhas
+         */
+    }
+}
