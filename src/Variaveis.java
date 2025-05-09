19410b8fda0a15037ce909dff98069f0eadfac97
EddieNine <believefixed@hotmail.com>
2025-05-09 18:42:44 -0300

    Variaveis em java

---
+++ src/Variaveis.java
@@ -0,0 +1,43 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        // Inteiros (int)
+        int idade = 30;
+        int ano = 2025;
+        int quantidade = 1;
+
+        // Doubles (double)
+        double preco = 19.99;
+        double temperatura = -5.5;
+        double gpa = 3.8;
+
+        // Caracteres (char)
+        char nota = 'A';
+        char simbolo = '!';
+        char moeda = '$';
+
+        // Booleanos (boolean)
+        boolean isStudent = true;
+        boolean isOnline = false;
+
+        if (isStudent) {
+            System.out.println("Você é um estudante.");
+        } else {
+            System.out.println("Você não é um estudante.");
+        }
+
+        // Strings (String)
+        String nome = "Bro Code";
+        String comida = "Pizza";
+        String email = "teste123@email.com";
+
+        /*  Dica Visual Extra: CamelCase
+            Usado para nomes de variáveis com mais de uma palavra:
+
+            boolean isStudent = true;
+            double averageScore = 9.5;
+        */
+
+
+    }
+}
